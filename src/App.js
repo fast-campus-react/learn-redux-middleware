864476442a573e40c5c2e0630b80b607db28cc02
bill <you@example.com>
2020-12-30 16:44:20 +0900

    redux-saga로 비동기 카운터 구현

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import PostPage from "./pages/PostPage";
 import PostListContainer from "./containers/PostListContainer";
-
+import CounterContainer from "./containers/CounterContainer";
 
 
 
 function App() {
   return (
     < >
+    <CounterContainer></CounterContainer>
        <Route path="/" component={PostListContainer} exact></Route>
        <Route path="/:id" component={PostPage} ></Route>
     </ >
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux";
-import counter from "./counter";
+import counter, { counterSaga } from "./counter";
 import posts from "./posts";
+import { all  } from "redux-saga/effects";
+
+
+export function* rootSaga(){
+    yield all([counterSaga()]);
+}
 
 export default combineReducers({ counter, posts });
